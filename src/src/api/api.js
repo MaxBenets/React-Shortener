2997f0026432e881c07e57cd0350e11620838385
Maksym <98233906+MaxBenets@users.noreply.github.com>
2022-07-17 16:00:32 +0300

    Delete src directory

--- src/src/api/api.js
+++
@@ -1,6 +0,0 @@
-import * as axios from "axios"
-
-export const ShortenLinkData = (link) => {
-    return axios.get("https://api.shrtco.de/v2/shorten?url="+link)
-        .then(responce => responce.data)
-}
--- src/src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./Header.module.css"
-
-export const Header = () => {
-    return(
-        <header className={s.header}>
-            <h1 className={s.h1}>React Shortener</h1>
-        </header>
-    )
-}
